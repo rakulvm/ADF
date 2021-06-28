452ff30312d43865e8736e6c8d274ac0014b734e
Rakul <rakulcareer6@gmail.com>
2021-06-28 07:52:30 +0530

    commit5

--- Day5TaskADF/new.py
+++
@@ -1,8 +0,0 @@
-def func_salary(sal_1):
-    """method"""
-    if 10000<= sal_1<=90000:
-        return "Eligible"
-    return "Not Eligible"
-a=func_salary(10000)
-print(a)
-# if start <= x <= end:
